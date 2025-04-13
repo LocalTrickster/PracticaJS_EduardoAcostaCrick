9dbc349c630c3736722e39af957d7e6a02f4cda1
LocalTrickster <eduardoacostacrick58@hotmail.com>
2025-04-13 00:47:16 -0300

    please work

---
+++ Ejercicio-5.js
@@ -0,0 +1,5 @@
+let  num1 = 8
+if (num1 % 2 == 0) {
+    num1 = "par"
+}  else num1 = "impar"
+console.log("el numero es " + num1);
---
+++ Ejercicio-7.js
@@ -0,0 +1,13 @@
+let num1= 6
+let num2= 23
+
+let resultado=("los numeros entre " + num1 + " y " + num2 + " son: " );
+
+for (let i = num1 +1; i < num2; i++) { 
+   
+    resultado += i + " ";
+    
+   
+ 
+}
+console.log(resultado);
